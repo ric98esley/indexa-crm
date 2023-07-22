cc6b1cd556dbab59a446671ba2ef6f79dbf6baf1
Ricardo Perez <ricardo.pereze.98@gmail.com>
2023-07-22 12:54:59 -0400

    [FETCH] (add asset form) agrega activos a la lista de activos a agregar

--- components/TabMenu.vue
+++ components/TabMenu.vue
@@ -24,36 +24,44 @@ interface Group {
   items: MenuItem[];
 }
 const props = defineProps({
-  menus: Array as PropType<Menu[]>
+  menus: Array as PropType<Menu[]>,
+  collapse: { type: Boolean, default: false }
 })
 
 </script>
 
 <template>
-  <el-aside width="13rem">
+  <div>
     <Logo width="w-32" margin="m-0" />
-    <el-scrollbar height="90%" noresize>
-      <el-menu :router="true">
-        <el-sub-menu v-for="(menu, indexMenu) in props.menus" :index="`${indexMenu}`">
-          <template #title>
-            <Icon :name="menu.icon" class="mx-1 text-lg" /> {{ menu.title }}
-          </template>
-          <el-menu-item-group v-for="(group, indexGroup) in menu.groups" :index="`g-${indexMenu}-${indexGroup}`">
+    <el-aside width="13rem">
+      <el-scrollbar height="90%" noresize>
+        <el-menu :router="true" :collapse="props.collapse">
+          <el-sub-menu v-for="(menu, indexMenu) in props.menus" :index="`${indexMenu}`">
             <template #title>
-              {{ group.title }}
+              <el-icon>
+                <Icon :name="menu.icon" class="mx-1 text-lg" />
+              </el-icon>
+              <span> {{ menu.title }} </span>
             </template>
-            <el-menu-item :index="`${indexMenu}-${indexGroup}`" v-for="menu in group.items" :route="menu.path">
-              {{ menu.title }}
-            </el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu v-if="menu.submenus" v-for="(submenu, indexSubmenu) in menu.submenus"
-            :index="`${indexMenu}-${indexSubmenu}`">
-            <template #title> <Icon :name="submenu.icon" class="mx-1 text-lg" />{{ submenu.title }}</template>
-            <el-menu-item v-for="(item, indexItem) in submenu.items " :route="item.path"
-              :index="`sub-${indexMenu}-${indexSubmenu}-${indexItem}`">{{ item.title }}</el-menu-item>
+            <el-menu-item-group v-for="(group, indexGroup) in menu.groups" :index="`g-${indexMenu}-${indexGroup}`">
+              <template #title>
+                {{ group.title }}
+              </template>
+              <el-menu-item :index="`${indexMenu}-${indexGroup}`" v-for="menu in group.items" :route="menu.path">
+                {{ menu.title }}
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu v-if="menu.submenus" v-for="(submenu, indexSubmenu) in menu.submenus"
+              :index="`${indexMenu}-${indexSubmenu}`">
+              <template #title>
+                <Icon :name="submenu.icon" class="mx-1 text-lg" />{{ submenu.title }}
+              </template>
+              <el-menu-item v-for="(item, indexItem) in submenu.items " :route="item.path"
+                :index="`sub-${indexMenu}-${indexSubmenu}-${indexItem}`">{{ item.title }}</el-menu-item>
+            </el-sub-menu>
           </el-sub-menu>
-        </el-sub-menu>
-      </el-menu>
-    </el-scrollbar>
-  </el-aside>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+  </div>
 </template>
--- pages/assets/add.vue
+++ pages/assets/add.vue
@@ -1,21 +1,171 @@
 <template>
-    <el-row :span="24">
-      <el-col :sm="24" :md="24" :lg="8">
-        <el-form label-position="top" label-width="auto">
-          <el-form-item label="serial">
-            <el-input placeholder="Serial">
+  <el-row :span="24" :gutter="20">
+    <el-col :span="24">
+      <h1 class="text-2xl m-4">Agregar activos al sistema</h1>
+    </el-col>
+    <el-col :sm="24" :md="24" :lg="8">
+      <el-card>
+        <el-form
+          label-position="top"
+          label-width="auto"
+          autocomplete="off"
+          ref="ruleFormRef"
+          :model="ruleForm"
+          :rules="rules"
+          >
+          <el-form-item label="Serial">
+            <el-input placeholder="Serial" v-model="asset.serial">
             </el-input>
           </el-form-item>
+          <el-form-item label="Modelo">
+            <el-select v-model="asset.modelId" filterable class="select-success" placeholder="Selecciona una modelo"
+              label="Modelo" style="width: 100%">
+              <el-option v-for="option in models" :key="option.id" :value="option.id"
+                :label="`${option.category.name} - ${option.name} - ${option.brand?.name}`">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Estatus del activo">
+            <el-select v-model="asset.stateId" class="select-success" placeholder="Selecciona un estado" label="Estado"
+              style="width: 100%" filterable>
+              <el-option v-for="option in status" :key="option.id" :value="option.id"
+                :label="`${option.id} - ${option.name}`">
+                {{ option.id }} - {{ option.name }}
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-row justify="space-between" align="middle">
+            <el-form-item>
+              <el-button type="primary" :disabled="!asset.serial || !asset.modelId || !asset.stateId" @click="addAsset()">Agregar</el-button>
+            </el-form-item>
+            <el-form-item>
+              <el-button link plain>Facturar</el-button>
+            </el-form-item>
+          </el-row>
         </el-form>
-      </el-col>
-    </el-row>
+      </el-card>
+    </el-col>
+    <el-col :sm="24" :md="24" :lg="16">
+      <el-card>
+        <el-table :data="toAdd.assets">
+          <el-table-column label="Serial">
+          </el-table-column>
+          <el-table-column label="Modelo">
+          </el-table-column>
+          <el-table-column label="Estado">
+          </el-table-column>
+          <el-table-column label="Acciones">
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
-<script>
+<script setup lang="ts">
+import type { FormInstance, FormRules } from 'element-plus';
+
 definePageMeta({
   middleware: [
     'nuxt-permissions'
   ],
-  roles: ['superuser', 'admin', 'tecnico'],
+  roles: ['superuser', 'admin', 'auditor'],
 })
+
+
+interface Asset {
+  serial?: string
+  modelId?: number
+  stateId?: number
+}
+
+interface Invoice {
+  id?: number,
+  code?: string,
+  providerId?: number,
+  total?: string,
+  invoiceDate?: string,
+}
+
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive<Asset>({
+  serial: undefined,
+  modelId: undefined,
+  stateId: undefined
+})
+
+const asset = reactive<Asset>({
+  serial: undefined,
+  modelId: undefined,
+  stateId: undefined,
+});
+
+const models = reactive([]);
+const status = reactive([]);
+
+const toAdd = reactive<{
+  assets: Asset[],
+  invoice: Invoice
+}>({
+  assets: [],
+  invoice: {
+    id: undefined,
+    code: undefined,
+    providerId: undefined,
+    total: undefined,
+    invoiceDate: undefined,
+  }
+});
+
+const rules = reactive<FormRules<Asset>>({
+  serial: [
+    {
+      required: true,
+      message: 'Serial invalido',
+      trigger: 'blur'
+    },
+    {
+      min: 3,
+      message: 'Minimo 3 caracteres',
+      trigger: 'blur'
+    },
+  ]
+
+})
+
+const getModels = async () => {
+  try {
+    const { data } = await useFetch<[]>('/assets/models');
+    return data.value
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getStatus = async () => {
+  try {
+    const { data } = await useFetch<[]>('/assets/status');
+    return data.value
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const addAsset = () => {
+  const newAsset = { ...asset };
+  if (!newAsset.serial || !newAsset.modelId || !newAsset.stateId) return;
+  if (toAdd.assets.find(({ serial }) => serial === newAsset.serial)) {
+    return;
+  }
+  asset.serial = '';
+  toAdd.assets.push(newAsset);
+}
+onMounted(async () => {
+  let modelsApi = await getModels() || [];
+  let statusApi = await getStatus() || [];
+  models.push(...modelsApi);
+  status.push(...statusApi);
+});
+
+
 </script>
