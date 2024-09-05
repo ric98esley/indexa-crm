18744d4e3d0602d22373a20ccad592c8ae9b1e76
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-09-05 08:33:55 -0400

    [FIX] (useGeo) repair offset

--- composables/useGeo.ts
+++ composables/useGeo.ts
@@ -1,9 +1,12 @@
-export const useFindGeo =  async (filters: FindGeoAlert)=> {
+export const useFindGeo = async (filters: FindGeoAlert) => {
   try {
-    const { data, error } = await useApi<{rows:GeoAlert[] , total: number}>('/geolocation', {
-      params:  useFilterObject(filters),
+    const { data, error } = await useApi<{ rows: GeoAlert[], total: number }>('/geolocation', {
+      params: useFilterObject({
+        ...filters,
+        offset: filters.offset > 0 ? (filters.offset - 1) * filters.limit : 0,
+      }),
     })
-    
+
     return data.value
   } catch (error) {
     ElNotification({
