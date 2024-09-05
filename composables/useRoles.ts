42a2734e029c3aa28be4502fa73670da2db3b6ad
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-09-05 08:38:07 -0400

    [UPDATE] (roles) add manament roles

--- composables/useRoles.ts
+++ composables/useRoles.ts
@@ -1,36 +1,36 @@
 export const useUserRoles = () => {
   const baseURL = '/roles';
   return class RoleService {
-    async find({}: {}) {
-      try {
-        const url = baseURL;
-        const { data, error } = await useApi<{total: number, rows: any}>(url, {
-          method: 'GET',
-        });
+    async find({ }: {}) {
+      const url = baseURL;
+      const { data, error } = await useApi<{ total: number, rows: any }>(url, {
+        method: 'GET',
+      });
 
-        return data.value
-      } catch (error : any) {
-        ElNotification({
-          title: 'Error al obtener los roles',
-          message: error.message,
-          type: 'error'
-        })
+      if (error.value) {
+        throw createError('Error al obtener roles');
       }
+
+      if (!data.value) {
+        throw createError('No se encontraron roles');
+      }
+
+      return data.value
     }
 
-    async create({name, ability}: {name: string, ability: Object }) {
+    async create({ name, ability }: { name: string, ability: Object }) {
       try {
         const body = useFilterObject({
           name,
           ability
         })
         const url = baseURL;
-        const { data, error } = await useApi<{total: number, rows: any}>(url, {
+        const { data, error } = await useApi<{ total: number, rows: any }>(url, {
           method: 'POST',
           body
         });
 
-        if(error.value) {
+        if (error.value) {
           throw new Error(error.value.data.message);
         }
 
@@ -38,13 +38,60 @@ export const useUserRoles = () => {
           title: 'Role creado correctamente',
           type: 'success'
         })
-      } catch (error : any) {
+      } catch (error: any) {
         ElNotification({
           title: 'error al crear rol',
           message: error.message,
           type: 'error'
         })
       }
+    }
+
+    async update(id: number, { name, ability }: { name: string, ability: Object }) {
+      const body = useFilterObject({
+        name,
+        ability
+      })
+      const url = `${baseURL}/${id}`;
+      const { data, error } = await useApi<{ total: number, rows: any }>(url, {
+        method: 'PATCH',
+        body
+      });
+
+      if (error.value) {
+        throw createError(error.value.data.message);
+      }
+
+      if (!data.value) {
+        throw createError('No se encontraron roles');
+      }
+
+      ElNotification({
+        title: 'Role actualizado correctamente',
+        type: 'success'
+      })
+
+    }
+
+    async delete(id: number) {
+      const url = `${baseURL}/${id}`;
+      const { data, error } = await useApi<{ total: number, rows: any }>(url, {
+        method: 'DELETE',
+      });
+
+      if (error.value) {
+        throw createError('Error al eliminar rol');
+      }
+
+      if (!data.value) {
+        throw createError('No se encontraron roles');
+      }
+
+      ElNotification({
+        title: 'Role eliminado correctamente',
+        type: 'success'
+      })
+
     }
   };
 };
--- pages/users/roles.vue
+++ pages/users/roles.vue
@@ -1,22 +1,3 @@
-<template>
-  <el-container direction="vertical" class="p-4">
-    <el-row :span="24" :gutter="20">
-      <el-col :span="24">
-        <PageHeader name="Roles" class="mb-4">
-          <template #buttons>
-            <div class="sm:flex items-center hidden">
-            </div>
-          </template>
-        </PageHeader>
-      </el-col>
-      <el-col :span="24">
-        <RolesTableView :data="userRoles.rows"/>
-      </el-col>
-      <RolesFormSave />
-    </el-row>
-  </el-container>
-</template>
-
 <script setup lang="ts">
 definePageMeta({
   middleware: [
@@ -26,6 +7,10 @@ definePageMeta({
 });
 const UserRoleServices = useUserRoles();
 const userRolesService = new UserRoleServices();
+
+const modals = reactive({
+  create: false,
+})
 
 const userRoles = reactive<{ total: number, rows: Role[] }>({
   total: 0,
@@ -38,8 +23,36 @@ const setRoles = async () => {
   userRoles.total = data?.total || 0;
 }
 
+const createRole = async (role: Role) => {
+  await userRolesService.create(role)
+  await setRoles()
+  modals.create = false
+}
+
 onMounted(async () => {
   await setRoles();
 })
 
 </script>
+
+<template>
+  <el-container direction="vertical" class="p-4">
+    <el-row :span="24" :gutter="20">
+      <el-col :span="24">
+        <PageHeader name="Roles" class="mb-4">
+          <template #buttons>
+            <div class="sm:flex items-center hidden">
+            </div>
+          </template>
+        </PageHeader>
+      </el-col>
+      <el-col :span="24">
+        <RolesTableView :data="userRoles.rows" @refresh="setRoles()" />
+      </el-col>
+      <el-dialog title="Crear Role" v-model="modals.create" destroy-on-close>
+        <RolesFormSave @submit="createRole" />
+      </el-dialog>
+    </el-row>
+    <LeftButton @click="modals.create = true" />
+  </el-container>
+</template>
